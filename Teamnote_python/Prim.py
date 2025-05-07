c2249a4f2672a6e7aea4a89262428c02c9be2679
rer10013 <parkedward03@gmail.com>
2025-05-07 15:47:30 +0900

    Prim-edit

--- Teamnote_python/Prim.py
+++ Teamnote_python/Prim.py
@@ -3,19 +3,20 @@ import heapq
 
 def prim(start, adj_list):
     visited = [False] * (N + 1)
-    min_heap = [(0, start)]
+    min_heap = [(0, start, -1)]
     mst_edges = []
 
     while min_heap:
-        weight, u = heapq.heappop(min_heap)
+        weight, u, prev = heapq.heappop(min_heap)
         if visited[u]:
             continue
         visited[u] = True
+        if prev != -1:
+            mst_edges.append((prev, u, weight))
 
         for v, w in adj_list[u]:
             if not visited[v]:
-                heapq.heappush(min_heap, (w, v))
-                mst_edges.append((u, v, w))
+                heapq.heappush(min_heap, (w, v, u))
 
     return mst_edges
 
@@ -29,3 +30,5 @@ for _ in range(M):
 
 edges = prim(1, adj_list)
 
+ans = sum(w for v, u, w in edges)
+print(ans)
