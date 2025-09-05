4fc074a1af25058cf72bd7e0031ad7d8e9b6daf4
rer10013 <parkedward03@gmail.com>
2025-09-06 01:44:12 +0900

    trie finish

--- Teamnote_python/Trie.py
+++ Teamnote_python/Trie.py
@@ -1,25 +1,32 @@
-'Trie'
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.end = False
-        
+        self.is_end_of_word = False
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-        
+
     def insert(self, word):
         node = self.root
-        for ch in word:
-            if ch not in node.children:
-                node.children[ch] = TrieNode()
-            node = node.children[ch]
-        node.end = True
-    
+        for i in word:
+            if i not in node.children:
+                node.children[i] = TrieNode()
+            node = node.children[i]
+        node.is_end_of_word = True
+
     def search(self, word):
         node = self.root
-        for ch in word:
-            if ch not in node.children:
+        for i in word:
+            if i not in node.children:
                 return False
-            node = node.children[ch]
-        return node.end
+            node = node.children[i]
+        return node.is_end_of_word
+
+    def startsWith(self, prefix):
+        node = self.root
+        for i in prefix:
+            if i not in node.children:
+                return False
+            node = node.children[i]
+        return True
